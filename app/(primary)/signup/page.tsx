3ae6cf5e678892529d7d4c36582ca77e64e12e4f
Ryan Zayne <phoenixastro97@gmail.com>
2024-06-09 01:05:15 +0100

    Refactor form pages to use dynamic import for code splitting

--- app/(primary)/signup/page.tsx
+++ app/(primary)/signup/page.tsx
@@ -7,6 +7,8 @@ import { useSearchParams } from "next/navigation";
 import { useForm } from "react-hook-form";
 import DropZoneInput from "./DropZoneInput";
 
+export const dynamic = 'force-dynamic'
+
 function SignUpPage() {
 	const methods = useForm({
 		defaultValues: {
