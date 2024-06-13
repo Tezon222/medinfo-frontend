f27e5738e0319478b25d2fec10e9ea347e6911f8
Ryan Zayne <phoenixastro97@gmail.com>
2024-06-13 18:05:45 +0100

    Feat: added progress bar

--- components/common/index.ts
+++ components/common/index.ts
@@ -1,5 +1,8 @@
 export { Slot, Slottable } from "./Slot";
 export { default as For } from "./For";
 export { default as Logo } from "./Logo/logo";
+// eslint-disable-next-line import-x/named
 export { IconBox } from "./IconBox";
 export { default as Show } from "./Show";
+export { default as Providers } from "./Providers";
+export { default as ProgressBar } from "./ProgressBar";
