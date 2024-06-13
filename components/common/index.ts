cdadb975763d8609e29062bb2222af5e9fb9344a
Ryan Zayne <phoenixastro97@gmail.com>
2024-06-13 22:30:08 +0100

    Improvements to select input

--- components/common/index.ts
+++ components/common/index.ts
@@ -1,7 +1,6 @@
 export { Slot, Slottable } from "./Slot";
 export { default as For } from "./For";
 export { default as Logo } from "./Logo/logo";
-// eslint-disable-next-line import-x/named
 export { IconBox } from "./IconBox";
 export { default as Show } from "./Show";
 export { default as Providers } from "./Providers";
