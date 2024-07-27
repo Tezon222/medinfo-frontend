026a4be41a3c6840a491644e24e6441781743ea9
johnpaul <edehjohnpaul@gmail.com>
2024-07-27 01:03:51 +0100

    Dashboard- profile and settings(50%)

--- app/(dashboard)/_components/SidebarLinks.tsx
+++ app/(dashboard)/_components/SidebarLinks.tsx
@@ -4,40 +4,36 @@ import MessageIcon from "@/components/icons/MessageIcon";
 import ProfileIcon from "@/components/icons/ProfileIcon";
 import SettingsIcon from "@/components/icons/SettingsIcon";
 
+interface MenuItem {
+	href: string;
+	title: string;
+	icon: (props: React.SVGProps<SVGSVGElement>) => JSX.Element;
+}
 
-// // TODO: Complete Sidebar urls
-// export const APP_URLS = {
-//   SERVICING: '/dashboard/gac/servicing',
-//   DRIVE_NOW_PAY_LATER: "'/dashboard/gac/drive-now-pay-later'",
-//   UPGRADE_RIDE: '/dashboard/gac/ride-upgrade',
-//   CONTACT_US: '/dashboard/gac/contact-us',
-//   TEST_DRIVE: '/dashboard/gac/test-drive',
-// } as const;
-
-export const menuItems = [
-  {
-    href: '/doctor',
-    title: 'Dashboard',
-    icon: DashboardIcon,
-  },
-  {
-    href: '/doctor/messages',
-    title: 'Messages',
-    icon: MessageIcon,
-  },
-  {
-	href: '/doctor/community',
-	title: 'Community',
-	icon: CommunityIcon,
- },
- {
-	href: '/doctor/profile',
-	title: 'Profile',
-	icon: ProfileIcon,
- },
- {
-	href: '/doctor/settings',
-	title: 'Settings',
-	icon: SettingsIcon,
- },
+export const menuItems: MenuItem[] = [
+	{
+		href: "/doctor",
+		title: "Dashboard",
+		icon: DashboardIcon,
+	},
+	{
+		href: "/doctor/messages",
+		title: "Messages",
+		icon: MessageIcon,
+	},
+	{
+		href: "/doctor/community",
+		title: "Community",
+		icon: CommunityIcon,
+	},
+	{
+		href: "/doctor/profile",
+		title: "Profile",
+		icon: ProfileIcon,
+	},
+	{
+		href: "/doctor/settings",
+		title: "Settings",
+		icon: SettingsIcon,
+	},
 ];
