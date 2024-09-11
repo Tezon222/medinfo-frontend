50d45ab8c9d554eae2a19dbfafe05fcd4f6323ef
Anagu Chidiebere Andrew <anaguchidiebere@gmail.com>
2024-09-11 01:39:12 +0100

    testing

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,9 @@ const nextConfig = {
 	eslint: {
 		ignoreDuringBuilds: true,
 	},
+	images: {
+		remotePatterns: [{ protocol: "https", hostname: "health.gov", port: "", pathname: "/**" }],
+	},
 };
 
 export default nextConfig;
